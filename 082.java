1b400e50c57c766cbf3d81978ec72fcf9cefd249
CodeWithJava <yhan14@stevens.edu>
2016-05-19 10:25:10 -0400

    FirstRound

---
+++ 082.java
@@ -0,0 +1,26 @@
+public class Solution
+{
+	public ListNode deleteDuplicates(ListNode head)
+	{
+		if(head == null || head.next == null)
+			return head;
+
+		ListNode dummyNode = new ListNode(0);
+		dummyNode.next = head;
+		ListNode p = dummyNode;
+
+		while(p.next != null && p.next.next != null)
+		{
+			if(p.next.val == p.next.next.val)
+			{
+				int dup = p.next.val;
+				while(p.next != null && p.next.val == dup)
+					p.next = p.next.next;
+			}
+			else
+				p = p.next;
+		}
+
+		return dummyNode.next;
+	}
+}
