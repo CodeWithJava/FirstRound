f216bb054567dcb900f637f71f5d3d62ebdd2c2d
nizeyu <zni1@stevens.edu>
2016-05-20 12:12:27 -0400

    FirstRound

--- DataStructure/LinkedList/148.java
+++ DataStructure/LinkedList/148.java
@@ -1,24 +1,70 @@
 public class Solution
 {
-	public ListNode insertionSortList(ListNode head)
-	{
-		if(head == null || head.next == null)
-			return;
+    public ListNode sortList(ListNode head)
+    {
+        if(head == null || head.next == null)
+            return head;
 
-		ListNode dummyNode = new ListNode(0);
+        int length = 0;
+        ListNode p = head;
+        while(p != null)
+        {
+            length++;
+            p = p.next;
+        }
 
-		while(head != null)
-		{
-			ListNode p = dummyNode;
-			while(p.next != null && p.next.val < head.val)
-				p = p.next;
+        int halfLength = length / 2;
 
-			ListNode t = head.next;
-			head.next = p.next;
-			p.next = head;
-			head = t;
-		}
+        ListNode l = head;
+        ListNode r = null;
+        p = head;
 
-		return dummyNode.next;
-	}
+        int half = 0;
+        while(p != null)
+        {
+            half++;
+            ListNode next = p.next;
+            if(half == halfLength)
+            {
+                p.next = null;
+                r = next;
+            }
+            p = next;
+        }
+
+        ListNode h1 = sortList(l);
+        ListNode h2 = sortList(r);
+        ListNode merged = merge(h1,h2);
+        return merged;
+    }
+
+    private ListNode merge(ListNode l1,ListNode l2)
+    {
+        if(l1 == null)
+            return l2;
+        if(l2 == null)
+            return l1;
+
+        ListNode dummyNode = new ListNode(0);
+        ListNode p = dummyNode;
+        
+        while(l1 != null && l2 != null)
+        {
+            if(l1.val < l2.val)
+            {
+                p.next = l1;
+                l1 = l1.next;
+            }
+            else
+            {
+                p.next = l2;
+                l2 = l2.next;
+            }
+            p = p.next;
+        }
+        if(l1 != null)  p.next = l1;
+        if(l2 != null)  p.next = l2;
+
+        return dummyNode.next;
+    }
 }
