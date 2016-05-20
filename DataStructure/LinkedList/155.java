4589a7990a326b1921b0b4ae161007a8c281b00d
CodeWithJava <yhan14@stevens.edu>
2016-05-20 17:01:26 -0400

    FirstRound

---
+++ DataStructure/LinkedList/155.java
@@ -0,0 +1,58 @@
+public class MinStack
+{
+	Node head;
+
+	// initialize your data structure here.
+	public MinStack()
+	{
+		head = null;
+	}
+
+	public void push(int x)
+	{
+		if(head == null)
+			head = new Node(x);
+		else
+		{
+			Node t = new Node(x);
+			t.min = Math.min(x,head.min);
+			t.next = head;
+			head = t;
+		}
+	}
+
+	public void pop()
+	{
+		if(head != null)
+			head = head.next;
+	}
+
+	public int top()
+	{
+		if(head != null)
+			return head.val;
+
+		throw new IllegalArgumentException("Empty stack");
+	}
+
+	public int getMin()
+	{
+		if(head != null)
+			return head.min;
+
+		throw new IllegalArgumentException("Empty stack");
+	}
+}
+
+class Node
+{
+	int val;
+	int min;
+	Node next;
+	Node(int x)
+	{
+		val = x;
+		min = x;
+		next = null;
+	}
+}
