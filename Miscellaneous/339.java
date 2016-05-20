ed3d8332363cee6a7b582c11be39ac0f806f0c87
CodeWithJava <yhan14@stevens.edu>
2016-05-19 23:08:17 -0400

    FirstRound

--- Miscellaneous/339.java
+++ Miscellaneous/339.java
@@ -12,6 +12,27 @@ Given the list [[1,1],2,[1,1]], return 10. (four 1's at depth 2, one 2 at depth 
 
 */
 
+/*
+
+ * // This is the interface that allows for creating nested lists.
+ * // You should not implement it, or speculate about its implementation
+ * public interface NestedInteger
+ * {
+ *
+ *     // @return true if this NestedInteger holds a single integer, rather than a nested list.
+ *     public boolean isInteger();
+ *
+ *     // @return the single integer that this NestedInteger holds, if it holds a single integer
+ *     // Return null if this NestedInteger holds a nested list
+ *     public Integer getInteger();
+ *
+ *     // @return the nested list that this NestedInteger holds, if it holds a nested list
+ *     // Return null if this NestedInteger holds a single integer
+ *     public List<NestedInteger> getList();
+ * }
+ 
+ */
+
 public class Solution
 {
 	public int depthSum(List<NestedInteger> nestedList)
