fbb9718924e7bf34bdba2ef89d02e1c9940bc36b
CodeWithJava <yhan14@stevens.edu>
2016-05-22 10:50:44 -0400

    FirstRound

--- DataStructure and Algorithm/Data Structure/Tree/235.java
+++ DataStructure and Algorithm/Data Structure/Tree/235.java
@@ -3,7 +3,7 @@ public class Solution
 	public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q)
 	{
 		if(root == null)
-			return null;
+			return root;
 
 		if(p.val < root.val && root.val < q.val)
 			return root;
