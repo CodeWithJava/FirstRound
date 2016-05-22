813c234d13eea7f612f51bb7ad4107de9bc6a8a9
CodeWithJava <yhan14@stevens.edu>
2016-05-22 10:49:22 -0400

    FirstRound

---
+++ DataStructure and Algorithm/Data Structure/Tree/235.java
@@ -0,0 +1,17 @@
+public class Solution
+{
+	public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q)
+	{
+		if(root == null)
+			return null;
+
+		if(p.val < root.val && root.val < q.val)
+			return root;
+		else if(p.val < root.val && q.val < root.val)
+			return lowestCommonAncestor(root.left,p,q);
+		else if(p.val > root.val && q.val > root.val)
+			return lowestCommonAncestor(root.right,p,q);
+
+		return root;
+	}
+}
