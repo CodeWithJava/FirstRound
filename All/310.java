87cdc0f5ad0c46e81a2a90f0c542c860ceddadb6
CodeWithJava <yhan14@stevens.edu>
2016-05-22 21:51:07 -0400

    FirstRound

---
+++ All/310.java
@@ -0,0 +1,92 @@
+public class Solution
+{
+	public List<Integer> findMinHeightTrees(int n, int [][] edges)
+	{
+		List<Integer> result = new ArrayList<>();
+
+		if(n == 1)
+		{
+			result.add(0);
+			return result;
+		}
+
+		List<List<Integer>> graph = new ArrayList<>(n);
+		for(int i = 0;i < n;i++)
+			graph.add(new ArrayList<>());
+
+		int [] inDegree = new int [n];
+
+		for(int [] edge: edges)
+		{
+			graph.get(edge[0]).add(edge[1]);
+			inDegree[edge[0]]++;
+			graph.get(edge[1]).add(edge[0]);
+			inDegree[edge[1]]++;
+		}
+
+		Queue<Integer> q = new LinkedList<>();
+		for(int i = 0;i < n;i++)
+			if(inDegree[i] == 1)
+				q.offer(i);
+
+		while(n > 2)
+		{
+			int size = q.size();
+			for(int i = 0;i < size;i++)
+			{
+				int x = q.poll();
+				n--;
+				for(int p: graph.get(x))
+				{
+					if(--inDegree[p])
+						q.offer(p);
+				}
+			}
+		}
+
+		while(!q.isEmpty())
+			result.add(q.poll());
+
+		return result;
+	}
+}
+
+public class Solution
+{
+	public List<Integer> findMinHeightTrees(int n, int [][] edges)
+	{
+		if(n == 1)
+			return Collections.singletonList(0);
+
+		List<Set<Integer>> graph = new ArrayList<>();
+		for(int i = 0;i < n;i++)
+			graph.add(new HashSet<>());
+
+		for(int [] edge: edges)
+		{
+			graph.get(edge[0]).add(edge[1]);
+			graph.get(edge[1]).add(edge[0]);
+		}
+
+		List<Integer> leaves = new ArrayList<>();
+		for(int i = 0;i < n;i++)
+			if(graph.get(i).size() == 1)
+				leaves.add(i);
+
+		while(n > 2)
+		{
+			n -= leaves.size();
+			List<Integer> newLeaves = new ArrayList<>();
+			for(int p: leaves)
+			{
+				int q = graph.get(p).iterator().next();
+				graph.get(q).remove(p);
+				if(graph.get(q).size() == 1)
+					newLeaves.add(q);
+			}
+			leaves = newLeaves;
+		}
+
+		return leaves;
+	}
+}
