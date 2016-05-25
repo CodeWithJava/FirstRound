e98bc0c99f6ab5776ee661af75b3c3180acd5ace
CodeWithJava <yhan14@stevens.edu>
2016-05-25 15:46:08 -0400

    FirstRound

--- All/031.java
+++ All/031.java
@@ -1,3 +1,7 @@
+// There are two direction for the permutation
+// previous permutation and next permutation
+// the leetcode problem is the next permutation
+// another one is the previous permutation
 public class Solution
 {
 	public void nextPermutation(int [] nums)
@@ -19,6 +23,65 @@ public class Solution
 		for(int i = nums.length - 1;i > p;i--)
 		{
 			if(nums[i] > nums[p])
+			{
+				q = i;
+				break;
+			}
+		}
+
+		if(p == 0 && q == 0)
+		{
+			reverse(nums,0,nums.length - 1);
+			return;
+		}
+
+		swap(nums,p,q);
+
+		if(p < nums.length - 1)
+			reverse(nums,p + 1,nums.length - 1);
+	}
+
+	private void swap(int [] nums, int i, int j)
+	{
+		int t = nums[i];
+		nums[i] = nums[j];
+		nums[j] = t;
+	}
+
+	private void reverse(int [] nums, int i, int j)
+	{
+		while(i < j)
+		{
+			int t = nums[i];
+			nums[i] = nums[j];
+			nums[j] = t;
+			i++;
+			j--;
+		}
+	}
+}
+
+public class Solution
+{
+	public void previousPermutation(int [] nums)
+	{
+		if(nums == null || nums.length == 0)
+			return;
+
+		int p = 0;
+		for(int i = nums.length - 1;i > 0;i--)
+		{
+			if(nums[i-1] > nums[i])
+			{
+				p = i - 1;
+				break;
+			}
+		}
+
+		int q = 0;
+		for(int i = nums.length - 1;i > p;i--)
+		{
+			if(nums[i] < nums[p])
 			{
 				q = i;
 				break;
