ffab5cb702a3fb978aa86d2c925bb20df99652be
CodeWithJava <yhan14@stevens.edu>
2016-05-25 23:26:15 -0400

    FirstRound

--- All/032.java
+++ All/032.java
@@ -1,8 +1,12 @@
+// There are two ways to implement the method
+// Must realisze the truth of the ways
+
+//Way One 
 public class Solution
 {
 	public int longestValidParentheses(String s)
 	{
-		if(s == null || s.length() == 0)
+		if(s == null || s.length() < 2)
 			return 0;
 
 		int max = 0;
@@ -24,6 +28,40 @@ public class Solution
 					stack.pop();
 					max = Math.max(max, stack.isEmpty() ? i + 1:i - stack.peek());
 				}
+			}
+		}
+
+		return max;
+	}
+}
+
+//Way Two
+public class Solution
+{
+	public int longestValidParentheses(String s)
+	{
+		if(s == null || s.length() < 2)
+			return 0;
+
+		int max = 0;
+		int [] dp = new int [s.length()];
+
+		for(int i = s.length() - 2;i >= 0;i--)
+		{
+			if(s.charAt(i) == '(')
+			{
+				int j = i + 1 + dp[i + 1];
+
+				if(j < s.length() && s.charAt(j) == ')')
+				{
+					dp[i] = dp[i + 1] + 2;
+
+					if(j + 1 < s.length())
+						dp[i] += dp[j + 1];
+				}
+
+				if(dp[i] > max)
+					max = dp[i];
 			}
 		}
 
