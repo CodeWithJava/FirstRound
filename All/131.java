b5b86f419665d7d82e74e16956ac4bd79102920d
CodeWithJava <yhan14@stevens.edu>
2016-05-28 14:37:52 -0400

    FirstRound

--- All/131.java
+++ All/131.java
@@ -51,3 +51,43 @@ public class Solution
 		return true;
 	}
 }
+
+// Dynamic Programming
+public class Solution
+{
+	public List<List<String>> partition(String s)
+	{
+		List<List<String>> [] result = new ArrayList [s.length() + 1];
+		result[0] = new ArrayList<>();
+		result[0].add(new ArrayList<>());
+
+		if(s == null || s.length() == 0)
+			return result[0];
+
+		int [][] dp = new int [s.length()][s.length()];
+
+		for(int r = 0;r < s.length();r++)
+		{
+			result[r + 1] = new ArrayList<>();
+
+			for(int l = 0;l <= r;l++)
+			{
+				if(s.charAt(l) == s.charAt(r) && (r - l < 2 || dp[l + 1][r - 1] == 1))
+				{
+					dp[l][r] = 1;
+
+					String str = s.substring(l,r + 1);
+
+					for(List<String> x: result[l])
+					{
+						List<String> t = new ArrayList<>(x);
+						t.add(str);
+						result[r + 1].add(t);
+					}
+				}
+			}
+		}
+
+		return result[s.length()];
+	}
+}
