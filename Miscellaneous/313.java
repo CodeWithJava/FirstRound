9de7d27fd3c6762b427e9c9e3695844efd176850
CodeWithJava <yhan14@stevens.edu>
2016-05-29 15:44:15 -0400

    FirstRound

---
+++ Miscellaneous/313.java
@@ -0,0 +1,25 @@
+public class Solution
+{
+	public int nthSuperUglyNumber(int n, int [] primes)
+	{
+		int [] index = new int [primes.length];
+		int [] result = new int [n];
+		result[0] = 1;
+
+		for(int i = 1;i < n;i++)
+		{
+			int minmum = Integer.MAX_VALUE;
+
+			for(int j = 0;j < primes.length;j++)
+				minmum = Math.min(minmum, primes[j] * result[index[j]]);
+
+			result[i] = minmum;
+
+			for(int j = 0;j < x.length;j++)
+				if(result[index[j]] * primes[j] == minmum)
+					index[j]++;
+		}
+
+		return result[n - 1];
+	}
+}
