234d1ef592acfa48833ef5eefd8becfc3b15bddd
CodeWithJava <yhan14@stevens.edu>
2016-06-11 18:43:00 -0400

    FirstRound

--- Miscellaneous/068.java
+++ Miscellaneous/068.java
@@ -1,80 +1,66 @@
 public class Solution
 {
-	public List<String> fullJustify(String [] words, int maxWidth)
+	public List<String> fullJustify(String[] words, int maxWidth)
 	{
 		List<String> result = new ArrayList<>();
 
 		if(words == null || words.length == 0)
-			return result;
+		return result;
 
-		int count = 0;
-		int last = 0;
+		int length = 0;
+		int lastWordIndex = 0;
 
 		for(int i = 0;i < words.length;i++)
 		{
-			count += words[i].length();
+			length += words[i].length();
 
-			if(count + i - last > maxWidth)
+			if(length + i - lastWordIndex > maxWidth)
 			{
-				int wordsLength = count - words[i].length();
+				int wordsLength = length - words[i].length();
 				int spaceLength = maxWidth - wordsLength;
-				int eachLength = 1;
+				int gapLength = 1;
 				int extraLength = 0;
 
-				if(i - last - 1 > 0)
+				if(i - 1 - lastWordIndex != 0)
 				{
-					eachLength = spaceLength / (i - last - 1);
-					extraLength = spaceLength % (i - last - 1);
+					gapLength = spaceLength / (i - 1 - lastWordIndex);
+					extraLength = spaceLength % (i - 1 - lastWordIndex);
 				}
 
 				StringBuilder sb = new StringBuilder();
 
-				for(int k = last; k < i - 1;k++)
+				for(int j = lastWordIndex;j < i - 1;j++)
 				{
-					sb.append(words[k]);
+					sb.append(words[j]);
 
-					int ce = 0;
-
-					while(ce < eachLength)
-					{
+					for(int k = 0;k < gapLength;k++)
 						sb.append(" ");
-						ce++;
-					}
 
-					if(extraLength > 0)
-					{
+					if(extraLength-- > 0)
 						sb.append(" ");
-						extraLength--;
-					}
 				}
 
-				sb.append(words[i - 1]);
+				sb.append(words[i -1]);
 
 				while(sb.length() < maxWidth)
 					sb.append(" ");
 
 				result.add(sb.toString());
 
-				last = i;
-				count = words[i].length();
+				lastWordIndex = i;
+				length = words[i].length();
 			}
 		}
 
-		int lastLength = 0;
 		StringBuilder sb = new StringBuilder();
 
-		for(int i = last;i < words.length - 1;i++)
-		{
-			count += words[i].length();
-			sb.append(words[i] + " ");
-		}
+		for(int i = lastWordIndex;i < words.length - 1;i++)
+		sb.append(words[i] + " ");
 
 		sb.append(words[words.length - 1]);
 
-		int d = 0;
-
 		while(sb.length() < maxWidth)
-			sb.append(" ");
+		sb.append(" ");
 
 		result.add(sb.toString());
 
