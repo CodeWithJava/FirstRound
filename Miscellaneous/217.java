0bfe67cba58b570e8a7825372d50c0089fe6c019
CodeWithJava <yhan14@stevens.edu>
2016-06-15 12:56:21 -0400

    FirstRound

---
+++ Miscellaneous/217.java
@@ -0,0 +1,19 @@
+// Time Limitation Exceed possible
+public class Solution
+{
+	public boolean containsDuplicate(int [] nums)
+	{
+		if(nums == null || nums.length <= 1)
+			return false;
+
+		Set<Integer> set = new HashSet<>();
+		for(int x: nums)
+		{
+			if(set.contains(x))
+				return true;
+			else
+				set.add(x);
+		}
+		return false;
+	}
+}
