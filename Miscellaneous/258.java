78f2506be9fc1842a13e8fc4a77e0df5f711cb1b
CodeWithJava <yhan14@stevens.edu>
2016-06-16 19:20:43 -0400

    FirstRound

---
+++ Miscellaneous/258.java
@@ -0,0 +1,16 @@
+public class Solution
+{
+    public int addDigits(int num)
+    {
+        if(num < 0)
+            throw new IllegalArgumentException("Input is invalid");
+
+        if(num == 0)
+            return 0;
+
+        if(num % 9 == 0)
+            return 9;
+
+        return num % 9;
+    }
+}
