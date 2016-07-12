8f24e8bd10ba96a3d2cd204f3e4e864052177e3e
CodeWithJava <yhan14@stevens.edu>
2016-07-12 16:55:57 -0400

    FirstRound

--- All/223.java
+++ All/223.java
@@ -1,16 +1,19 @@
 public class Solution
 {
-    public int computeArea(int A, int B, int C, int D, int E, int F, int G, int H)
-    {
-        int result = (C - A) * (D - B) + (G - E) * (H - F);
-        if(C < E || G < A)
-            return result;
-        if(D < F || H <B)
-            return result;
-        int top = Math.min(H,D);
-        int bottom = Math.max(F,B);
-        int left = Math.max(A,E);
-        int right = Math.min (C,G);
-        return result - (right - left) * (top - bottom);
-    }
+	public int computeArea(int A, int B, int C, int D, int E, int F, int G, int H)
+	{
+		int result = (C - A) * (D - B) + (G - E) * (H - F);
+
+		if(C < E || G < A)
+		return result;
+		if(D < F || H < B)
+		return result;
+
+		int top = Math.min(H,D);
+		int bottom = Math.max(F,B);
+		int left = Math.max(A,E);
+		int right = Math.min (C,G);
+
+		return result - (right - left) * (top - bottom);
+	}
 }
